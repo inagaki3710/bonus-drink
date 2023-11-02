ac61fb5ea5865c5a08535dfe3fe074342e2c4fc2
inagaki3710 <149679545+inagaki3710@users.noreply.github.com>
2023-11-02 21:44:46 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+    while amount >= 3
+      exchange = amount / 3
+      total += exchange
+      amount = exchange + amount % 3
+    end
+    total
   end
 end
+
+puts BonusDrink.total_count_for(100)
